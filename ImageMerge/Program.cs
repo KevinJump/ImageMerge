7594a3fce48e6152ed17de56a7dcd08939ed6fd8
Kevin Jump <kevin@thejumps.co.uk>
2014-03-12 09:57:29 +0000

    Initial Commit

---
+++ ImageMerge/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace ImageMerge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string imagefolder = "";
+            if (args.Count() == 1)
+            {
+                imagefolder = args[0];
+            }
+            else
+            {
+                Console.WriteLine("ImageMerge <path>\n");
+                Console.WriteLine("go through a folder of images, and make a merged overlay image of all the images");
+            }
+            
+
+            int width = 0;
+            int height = 0;
+            int count = 0 ; 
+
+            foreach(string file in Directory.GetFiles(imagefolder, "*_desktop.jpg" )) 
+            {
+                Console.Write(".");
+
+                count++;
+
+                System.Drawing.Bitmap image = new Bitmap(file);
+
+                if (image.Width > width)
+                    width = image.Width;
+
+                if (image.Height > height)
+                    height = image.Height;
+            }
+
+            Console.WriteLine("Max Image: {0} x {1}", width, height);
+
+            Bitmap final = new Bitmap(width, height);
+            Graphics g = Graphics.FromImage(final);
+            g.Clear(Color.White);
+
+            Console.WriteLine("Processing {0}", count);
+
+            foreach (string file in Directory.GetFiles(imagefolder, "*_desktop.jpg"))
+            {
+                Console.Write(".");
+                System.Drawing.Bitmap image = new Bitmap(file);
+
+                image.MakeTransparent(Color.White);
+                
+                Bitmap newImage = new Bitmap(SetImageOpacity((Image)image, 0.003F));
+
+                g.DrawImage(newImage, new Point(0, 0));
+            }
+
+            Console.WriteLine(" Done.");
+
+            string saveFile = String.Format("{0}\\output\\desktop_all_{1}.png", imagefolder, DateTime.Now.ToString("hhss"));
+            
+            if (!Directory.Exists(Path.GetDirectoryName(saveFile)))
+                Directory.CreateDirectory(Path.GetDirectoryName(saveFile));
+
+            final.Save(saveFile, ImageFormat.Png);
+        }
+
+        /// <param name="opacity">percentage of opacity</param>  
+        /// <returns></returns>  
+        public static Image SetImageOpacity(Image image, float opacity)
+        {
+            try
+            {
+                //create a Bitmap the size of the image provided  
+                Bitmap bmp = new Bitmap(image.Width, image.Height);
+
+                //create a graphics object from the image  
+                using (Graphics gfx = Graphics.FromImage(bmp))
+                {
+
+                    //create a color matrix object  
+                    ColorMatrix matrix = new ColorMatrix();
+
+                    //set the opacity  
+                    matrix.Matrix33 = opacity;
+
+                    //create image attributes  
+                    ImageAttributes attributes = new ImageAttributes();
+
+                    //set the color(opacity) of the image  
+                    attributes.SetColorMatrix(matrix, ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
+
+                    //now draw the image  
+                    gfx.DrawImage(image, new Rectangle(0, 0, bmp.Width, bmp.Height), 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, attributes);
+                }
+                return bmp;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return null;
+            }
+        } 
+    }
+}
---
+++ ImageTile/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+
+namespace ImageTile
+{
+    class Program
+    {
+
+        /// <summary>
+        ///  tile a load of screenshots into one huge image...
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            string imagefolder = "";
+            if (args.Count() == 1)
+            {
+                imagefolder = args[0];
+            }
+            else
+            {
+                Console.WriteLine("ImageTile <path>\n");
+                Console.WriteLine("go through a folder of images, and make a large tiled image\nworks best if all the images are the samesize");
+            }
+            int acrossCount = 23;
+
+            int width = 0;
+            int height = 0;
+            int count = 0;
+
+            foreach (string file in Directory.GetFiles(imagefolder, "*_desktop.jpg"))
+            {
+                Console.Write(".");
+
+                count++;
+
+                System.Drawing.Bitmap image = new Bitmap(file);
+
+                if (image.Width > width)
+                    width = image.Width;
+
+                if (image.Height > height)
+                    height = image.Height;
+            }
+
+            int imageWidth = width * acrossCount;
+
+                
+            int rows = (int)Math.Ceiling( (double)(count / acrossCount));
+            int depth = height * (rows +1);
+
+            Console.WriteLine("Image Will be {0} x {1}", imageWidth, depth);
+
+            Bitmap final = new Bitmap(imageWidth, depth);
+            Graphics g = Graphics.FromImage(final);
+            g.Clear(Color.White);
+
+            Console.WriteLine("Processing {0}", count);
+            int processed = 0; 
+
+            foreach (string file in Directory.GetFiles(imagefolder, "*_desktop.jpg"))
+            {
+                Console.Write(".");
+                System.Drawing.Bitmap image = new Bitmap(file);
+
+                int x = (processed % acrossCount) * width ;
+                int y = (int)Math.Ceiling( (double)(processed / acrossCount)) * height;
+
+                Console.WriteLine("{0} on Col:{1} Row:{2} At {3}, {4}", processed, (processed % acrossCount),(int)Math.Ceiling( (double)(processed / acrossCount)),  x, y);
+
+                g.DrawImage(image, new Point(x,y));
+                
+                processed++;
+            }
+
+            Console.WriteLine(" Done.");
+
+            string saveFile = String.Format("{0}\\output\\tiles_desktop_{1}.png", imagefolder, DateTime.Now.ToString("hhss"));
+
+            if ( !Directory.Exists( Path.GetDirectoryName(saveFile)))
+                Directory.CreateDirectory(Path.GetDirectoryName(saveFile));
+
+            final.Save(saveFile, ImageFormat.Png);
+        }
+    }
+}
